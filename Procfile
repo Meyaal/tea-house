50e5cbf920342ea3b081ef7281498df481659735
Meyaal <alsuhaili_32@hotmail.com>
2023-08-11 08:55:21 +0000

    create procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn teahouse.wsgi --log-file -
--- teahouse/teahouse/settings.py
+++ teahouse/teahouse/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-meyaal-teahouse-r9n8ty34ek6.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['8000-meyaal-teahouse-r9n8ty34ek6.ws-eu102.gitpod.io', 'teahouse.herokuapp.com']
 
 
 # Application definition
