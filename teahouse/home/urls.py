925db3c1de15523b31f5a31e5d5c4d5f939eea29
Meyaal <alsuhaili_32@hotmail.com>
2023-09-01 14:03:55 +0000

    added html and structured it

--- teahouse/home/urls.py
+++ teahouse/home/urls.py
@@ -4,5 +4,12 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('about/', views.about, name='about'),
+    path('feature/', views.feature, name='feature'),
+    path('products/', views.products, name='products'),
+    path('store/', views.store, name='store'),
+    path('blog/', views.blog, name='blog'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('404/', views.notFound, name='404'),
+    path('contact', views.contact, name='contact')
 ]
